f5c5f78d79273b6dafd2bd522dbe5d6649f57dd1
Tutoz <tutos_aaf@hotmail.com>
2023-06-28 17:06:14 -0300

    Validaciones para el registro de contraseña.

--- Talentium/LogicaNegocio/Validaciones.cs
+++ Talentium/LogicaNegocio/Validaciones.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LogicaNegocio
@@ -18,6 +19,35 @@ namespace LogicaNegocio
         public static int GetIntentos() { return intentos; }
         public static DateTime GetHoraDesbloqueo() { valIntentos(); return horaBloqueo.AddMinutes(lapsoBloqueo); }
 
+// VALIDA USUARIO Y CONTRASEÑA.
+        // MANEJA LOS INTENTOS DE CONTRASEÑA INCORRECTA.
+        private static void valIntentos()
+        {
+            if (intentos > 0)
+            {
+                intentos--; // Resta un intento.
+            }
+            else if (bloqueado == false)
+            {
+                bloqueado = true;
+                horaBloqueo = System.DateTime.Now; // Bloquea y marca la hora de bloqueo.
+            }
+            else
+            {
+                reactivar(); // Chekea si ya paso el lapso de bloqueo.
+            }
+        }
+        // CHEKEA SI SE DEBE O NO DESBLOQUEAR EL USUARIO.
+        private static void reactivar()
+        {
+            if (bloqueado == false);
+            else if (DateTime.Now > horaBloqueo.AddMinutes(lapsoBloqueo)) // Si ya paso el tiempo de bloqueo;
+            {
+                bloqueado = false;
+                intentos = 5; // Desbloquea y restablece los intentos.
+            }
+        }
+        // VALIDACION FINAL DE USUARIO Y CONTRASEÑA
         public static bool ValUsr(string usuario, string password)
         {
             bool pswVal = false, pswDigVal = false;
@@ -31,46 +61,64 @@ namespace LogicaNegocio
             string digitoForm = Seguridad.Hash(Seguridad.DigVerif(Seguridad.Hash(password)).ToString());
             string digitoBd = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9";
 
-            if (comparar(usrForm, usrBd))
+            if (string.Equals(usrForm, usrBd))
             {
-                if (comparar(pswBd, pswForm)) pswVal = true;
+                if (string.Equals(pswBd, pswForm)) pswVal = true;
                 else valIntentos();
             }
-            if (comparar(digitoBd, digitoForm)) pswDigVal = true;
+            if (string.Equals(digitoBd, digitoForm)) pswDigVal = true;
 
             return pswVal && pswDigVal && !bloqueado;
         }
-        private static bool comparar(string x1, string x2)
-        {
-            return x1 == x2;
-        }
-        private static void valIntentos()
-        {
-            if (intentos > 0)
-            {
-                intentos--;
-            }
-            else if (bloqueado == false)
-            {
-                bloqueado = true;
-                horaBloqueo = System.DateTime.Now;
-            }
-            else
-            {
-                reactivar();
-            }
-        }
-        private static void reactivar()
-        {
-            if (bloqueado == false)
-            {
 
-            }
-            else if (DateTime.Now > horaBloqueo.AddMinutes(lapsoBloqueo))
-            {
-                bloqueado = false;
-                intentos = 5;
-            }
+
+// VALIDAR CARACTERES
+        // VALIDA SI TIENE MINIMO 8 CARACTERES.
+        private static bool minimoDeCaracteres(string str)
+        {
+            if (str.Length >= 8) return true;
+            return false;
+        }
+        // VALIDA SI TIENE MAYUSCULAS.
+        private static bool tieneMayuscula(string str)
+        {
+            string mayusculas = @"[A-Z]";
+            if (Regex.IsMatch(str, mayusculas)) return true;
+            return false;
+        }
+        // VALIDA SI TIENE MINUSCULAS.
+        private static bool tieneMinuscula(string str)
+        {
+            string mayusculas = @"[a-z]";
+            if (Regex.IsMatch(str, mayusculas)) return true;
+            return false;
+        }
+        // VALIDA SI TIENE NUMEROS.
+        private static bool tieneNumero(string str)
+        {
+            string numeros = @"[0-9]";
+            if (Regex.IsMatch(str, numeros)) return true;
+            return false;
+        }
+        // VALIDA SI TIENE CARACTERES ESPECIALES.
+        private static bool tieneCaracterEspecial(string str)
+        {
+            string numeros = @"[^\w \-]";
+            if (Regex.IsMatch(str, numeros)) return true;
+            return false;
+        }
+        // VALIDACION FINAL DE CARACTERES: SE ELIGE QUE VALIDACIONES SE USAN, Y SI LAS SELECCIONADAS SON true DEVUELVE true.
+        public static bool ValCar(string str, bool minCaracteres, bool mayusculas, bool numeros, bool minusculas, bool especiales)
+        {
+            bool minCar, may, num, min, esp;
+
+            if (minCaracteres) minCar = minimoDeCaracteres(str); else minCar = true;
+            if (mayusculas) may = tieneMayuscula(str); else may = true;
+            if (numeros) num = tieneNumero(str); else num = true;
+            if (minusculas) min = tieneMinuscula(str); else min = true;
+            if (especiales) esp = tieneCaracterEspecial(str); else esp = true;
+
+            return minCar && may && num && min && esp;
         }
     }
 }
