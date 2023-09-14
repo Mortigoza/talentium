2e6a947e131ff1f59f7f89a03730fe68c0a4bd50
maira rodriguez <mairaaracelirodriguez@gmail.com>
2023-09-13 23:21:20 -0300

    modificacion del git ignore a la rama

--- Talentium/Vista/frmRecupero.Designer.cs
+++ Talentium/Vista/frmRecupero.Designer.cs
@@ -127,7 +127,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(243, 337);
+            this.label4.Location = new System.Drawing.Point(252, 328);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 16);
             this.label4.TabIndex = 8;
