68337bdf1bd04f4c28cd578bc6fa27163150121a
diana noelia <diananoelia.diaz@gmail.com>
2024-03-07 02:59:27 -0300

    fix reactivar

--- Talentium/Vista/Administracion Personal/frmConsultaPersonal.cs
+++ Talentium/Vista/Administracion Personal/frmConsultaPersonal.cs
@@ -206,7 +206,8 @@ namespace Vista
 
                                 frmAltaPersonal frmAltaPersonal = new frmAltaPersonal(false);
                                 frmAltaPersonal.EsReactivacion = true; // Es una reactivación
-                                frmAltaPersonal.CargarDatosModificacion(idper, true);
+                                frmAltaPersonal.CargarDatosModificacion(idper, false, true);
+                                frmAltaPersonal.dttFechaAlta.Enabled = true;
                                 frmAltaPersonal.ShowDialog();
 
                             }
